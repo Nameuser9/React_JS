b4bf52629cf31d4bc97bb976a9b5283c0a80437d
Nameuser9 <ivamand2002@gmail.com>
2024-11-29 00:25:19 +0300

    hw0.2

--- HW8/src/index.js
+++ HW8/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App';    
 import CartContextProvider from './contexts/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
